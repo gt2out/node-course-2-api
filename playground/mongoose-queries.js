a737c49066faaa31f0610a469486fef9805e0eaa
unknown <gt2out@hotmail.com>
2020-08-07 11:44:07 -0400

    Add GET /todos/:id

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -1,3 +1,5 @@
+//Lesson 78
+
 const {ObjectID} = require('mongodb');
 const {mongoose} = require('./../server/db/mongoose');
 const {Todo} = require('./../server/models/todo');
--- server/server.js
+++ server/server.js
@@ -2,10 +2,12 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+
 
 var app = express();
 
@@ -29,6 +31,31 @@ app.get('/todos', (req, res) => {
     }, (e) => {
        res.status(400).send(e);
     });
+});
+
+// GET /todos/1234132
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    
+    //validate using is valid
+    if (!ObjectID.isValid(id)) {
+         // 404 response - empty send
+        return res.status(404).send();
+    };
+
+    //find by id - if todo send it back
+    Todo.findById(id).then((todo) => {
+        if (todo) {    
+        res.send({todo});
+        } else {
+            res.status(404).send();
+        }   
+    }, (e) => {
+        res.status(404).send(e);
+    });
+    //if no todo - send 404 with empty body
+    //success
+    //error - send 400 - send empty body
 });
 
 app.listen(3000, () => {

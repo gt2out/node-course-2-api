9aedca695015eb090bd3244851f406c571b44910
unknown <gt2out@hotmail.com>
2020-08-12 09:52:12 -0400

    Set up different test and development envs

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 // Lesson 85
+require('./config/config');
 
 const _ = require('lodash')
 const express = require('express');
@@ -11,7 +12,7 @@ var {User} = require('./models/user');
 
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
